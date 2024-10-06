713f2ddbf2c462001cccaed72790cc50ffecdf8f
Kauah <menezeskauah@gmail.com>
2024-10-05 23:20:52 -0300

    cria menu

--- src/components/template/Menu.tsx
+++ src/components/template/Menu.tsx
@@ -1,3 +1,5 @@
+import MenuGrupo from "./MenuGrupo"
+import MenuItem from "./MenuItem"
 
 
 export interface MenuProps {
@@ -6,8 +8,34 @@ export interface MenuProps {
 
 export default function Menu(props:MenuProps) {
     return (
-        <aside className={`${ props.className ?? '' }`}>
-            <nav>Menu</nav>
+        <aside className={`px-7 ${ props.className ?? '' }`}>
+            <nav className="flex flex-col gap-6">
+                <MenuGrupo label="Box Model">
+                    <MenuItem label="Padding" href="/padding"/>
+                    <MenuItem label="Margin" href="/margin"/>
+                    <MenuItem label="Border" href="/border"/>
+                </MenuGrupo>
+                <MenuGrupo label="Display">
+                    <MenuItem label="Block" href="/block"/>
+                    <MenuItem label="Inline" href="/inline"/>
+                    <MenuItem label="Inline Block" href="/inline-block"/>
+                </MenuGrupo>
+                <MenuGrupo label="Flexbox">
+                    <MenuItem label="Flex Container" href="/flex-container"/>
+                    <MenuItem label="Flex Item" href="/flex-item"/>
+                    <MenuItem label="Flex Direction" href="/flex-direction"/>
+                </MenuGrupo>
+                <MenuGrupo label="Grid">
+                    <MenuItem label="Grid Container" href="/grid-container"/>
+                    <MenuItem label="Grid Item" href="/grid-item"/>
+                    <MenuItem label="Grid Template Columns" href="/grid-template-columns"/>
+                </MenuGrupo>
+                <MenuGrupo label="Responsividade">
+                    <MenuItem label="Breakpoints" href="/breakpoints"/>
+                    <MenuItem label="Media Query" href="/media-query"/>
+                    <MenuItem label="Mobile First" href="/mobile-first"/>
+                </MenuGrupo>
+            </nav>
         </aside>
     )
 }
---
+++ src/components/template/MenuGrupo.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+
+export interface MenuGrupoProps {
+    label:string;
+    children?:ReactNode;
+    className?:string;
+};
+
+export default function MenuGrupo(props:MenuGrupoProps) {
+    return (
+        <div 
+            className={`
+                flex flex-col gap-2
+                ${ props.className ?? '' }
+            `}
+        > 
+            <span className="text-white font-semibold">{ props.label }</span>
+            <div>
+                { props.children }
+            </div>
+        </div>
+    );
+};
